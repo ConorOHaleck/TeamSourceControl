197d070fdfb165f7a02da8ad6382c9eba85a701e
Conor O'Haleck <conorohaleck@gmail.com>
2020-02-06 11:34:35 -0800

    Added class for Army object

---
+++ TeamSourceControl_AgeofEmpires/Army.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceControl_AgeofEmpires
+{
+    class Army
+    {
+        public int ArmyID { get; set; }
+        public int MyProperty { get; set; }
+        public String ArmyName { get; set; }
+        public String ArmyOwner { get; set; }
+
+        public ICollection<ArmySoldier> ArmySoldiers { get; set; }
+    }
+}
