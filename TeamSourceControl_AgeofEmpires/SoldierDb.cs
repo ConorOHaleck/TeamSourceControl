5a0c12690ace3948191a711e66326e83645af727
Jacob Gjerstad <jgjerstad@comcast.net>
2020-02-06 21:36:23 -0800

    Added select functionality for Armies and Soldiers

---
+++ TeamSourceControl_AgeofEmpires/SoldierDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceControl_AgeofEmpires
+{
+    class SoldierDb
+    {
+        /// <summary>
+        /// Returns a list of all the soldiers
+        /// </summary>
+        public static List<Soldier> GetAllSoldiers()
+        {
+            AOEContext context = new AOEContext();
+
+            List<Soldier> soldiers = (from s in context.Soldiers
+                                      select s).ToList();
+
+            return soldiers;
+        }
+    }
+}
