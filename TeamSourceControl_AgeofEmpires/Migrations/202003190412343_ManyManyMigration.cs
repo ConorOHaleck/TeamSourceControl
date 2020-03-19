beee1e5ba6408a15516bb9fecd61d884382e658b
ofHillsAndLakes <conorohaleck@gmail.com>
2020-03-18 21:15:00 -0700

    Created new correctly implemeneted Many to Many Relationship

---
+++ TeamSourceControl_AgeofEmpires/Migrations/202003190412343_ManyManyMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace TeamSourceControl_AgeofEmpires.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ManyManyMigration : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Armies", "MyProperty");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Armies", "MyProperty", c => c.Int(nullable: false));
+        }
+    }
+}
